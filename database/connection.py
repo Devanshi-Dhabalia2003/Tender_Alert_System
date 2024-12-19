60d352223531a2e3d979b9d812b132cf4dbd7548
Devanshi Dhabalia <87478080+Devanshi-Dhabalia2003@users.noreply.github.com>
2024-12-19 15:51:52 +0530

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -10,7 +10,13 @@ server = os.getenv("DB_SERVER")
 database = os.getenv("DB_NAME")
 driver = os.getenv("DB_DRIVER")
 trusted_connection = os.getenv("DB_TRUSTED_CONNECTION")
-connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection={trusted_connection};Timeout=30;"
+connection_string = (
+    f"DRIVER={driver};"
+    f"SERVER={server};"
+    f"DATABASE={database};"
+    f"Timeout=30;"  # Increase the timeout to 30 seconds
+    f"Trusted_Connection={trusted_connection};"
+)
 
 
 # Create connection
