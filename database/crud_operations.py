ab37cae4dd897dbf8dc067b719c10ddd41bd3d25
Devanshi Dhabalia <87478080+Devanshi-Dhabalia2003@users.noreply.github.com>
2024-12-19 15:33:08 +0530

    Update crud_operations.py

--- database/crud_operations.py
+++ database/crud_operations.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from database.connection import get_sql_connection
 
 # Function to fetch filtered records
