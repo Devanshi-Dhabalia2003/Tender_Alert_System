379fb4b4240c5be7ea88e3b6a2649bf0f2175c85
Devanshi Dhabalia <87478080+Devanshi-Dhabalia2003@users.noreply.github.com>
2024-12-19 15:50:22 +0530

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -10,7 +10,7 @@ server = os.getenv("DB_SERVER")
 database = os.getenv("DB_NAME")
 driver = os.getenv("DB_DRIVER")
 trusted_connection = os.getenv("DB_TRUSTED_CONNECTION")
-connection_string = f"DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={server};DATABASE={database};Trusted_Connection={trusted_connection};Timeout=30;"
+connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection={trusted_connection};Timeout=30;"
 
 
 # Create connection
