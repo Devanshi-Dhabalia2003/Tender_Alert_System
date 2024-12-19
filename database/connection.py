ff51e44bcfd90ef724620026e76540df1f64fd07
Devanshi Dhabalia <87478080+Devanshi-Dhabalia2003@users.noreply.github.com>
2024-12-19 15:31:39 +0530

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -2,16 +2,16 @@ import pyodbc
 import os
 from dotenv import load_dotenv
 
+# Load .env file
 load_dotenv()
 
+# SQL Server Connection Setup
 server = os.getenv("DB_SERVER")
 database = os.getenv("DB_NAME")
 driver = os.getenv("DB_DRIVER")
 trusted_connection = os.getenv("DB_TRUSTED_CONNECTION")
 connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};Trusted_Connection={trusted_connection};"
 
-try:
-    conn = pyodbc.connect(connection_string)
-    print("Connection successful!")
-except Exception as e:
-    print(f"Connection failed: {e}")
+# Create connection
+def get_sql_connection():
+    return pyodbc.connect(connection_string)
